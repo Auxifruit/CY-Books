b108bcea662d851b33d729395f8235898fe4689f
Guillaume B <105287640+Auxifruit@users.noreply.github.com>
2024-05-14 18:12:56 +0200

    Nouvelle version + code mieux organisé

--- CYBooks/src/presentation/UsersManagement.java
+++ CYBooks/src/presentation/UsersManagement.java
@@ -11,6 +11,8 @@ import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -49,38 +51,65 @@ public class UsersManagement extends Application {
 	 */
 	@Override
 	public void start(Stage stage) throws Exception {
-		// HBox containing all the button to change scene
-		HBox hboxButtonChangeScene = new HBox(10);
-		
-		// Buttons to change scene
-		Button goToUsersTableButton = new Button("Users table");
-		goToUsersTableButton.setOnAction(e -> {
-			usersBorderPane.setCenter(usersTablePane);
-	    });
-		
-		Button goToUserCreationButton = new Button("Create user");
-	    goToUserCreationButton.setOnAction(e -> {
-	    	usersBorderPane.setCenter(usersCreationPane);
-	    });
-	    
-	    Button goToUserModificationButton = new Button("Modify user");
-	    goToUserModificationButton.setOnAction(e -> {
-	    	usersBorderPane.setCenter(usersModificationPane);
-	    });
-
-	    hboxButtonChangeScene.getChildren().addAll(goToUsersTableButton, goToUserCreationButton, goToUserModificationButton);
-	    
-		// Users Table pane
-	    usersTablePane = new Pane();
-		
-	    // We initialize the values of all the columns
+		// We initialize the values of all the columns
 	    initializeCol();
 		
 		stage.setTitle("Users table view");
 	    stage.setWidth(800);
 	    
-	    final Label label = new Label("Users");
-	    label.setFont(new Font("Arial", 20));
+		// Users Table pane
+	    usersTablePane = new VBox();
+	    
+	    usersTablePane.getChildren().add(createUsersTablePane());
+	    
+	    // Users Creation pane
+	    usersCreationPane = new VBox();
+	    
+	    usersCreationPane.getChildren().addAll(createUserCreationPane());
+
+	    // Users Modification pane
+	    usersModificationPane = new VBox();
+	    
+	    usersModificationPane.getChildren().addAll(createUserModificationPane());
+	    
+    	// HBox containing all the button to change scene
+ 		HBox hboxButtonChangeScene = new HBox(50);
+ 		hboxButtonChangeScene.setAlignment(Pos.CENTER);
+ 		hboxButtonChangeScene.setPrefHeight(50);
+ 		
+ 		// Buttons to change scene
+ 		Button goToUsersTableButton = new Button("Users table");
+ 		goToUsersTableButton.setOnAction(e -> {
+ 			usersBorderPane.setCenter(usersTablePane);
+ 	    });
+ 		
+ 		Button goToUserCreationButton = new Button("Create an user");
+ 	    goToUserCreationButton.setOnAction(e -> {
+ 	    	usersBorderPane.setCenter(usersCreationPane);
+ 	    });
+ 	    
+ 	    Button goToUserModificationButton = new Button("Modify the user");
+ 	    goToUserModificationButton.setOnAction(e -> {
+ 	    	usersBorderPane.setCenter(usersModificationPane);
+ 	    });
+
+ 	    hboxButtonChangeScene.getChildren().addAll(goToUsersTableButton, goToUserCreationButton, goToUserModificationButton);
+	    
+	    // Main scene of the application
+	    usersBorderPane.setCenter(usersTablePane);
+	    usersBorderPane.setBottom(hboxButtonChangeScene);
+	    stage.setScene(new Scene(usersBorderPane));
+	    stage.show();
+	}
+
+	private Pane createUsersTablePane() {
+		// VBox containing all the node for the users table expect the main label
+		VBox searchTableButtonVBox = new VBox(20);
+		
+	    Label labelUserTable = new Label("USERS TABLE: ");
+		labelUserTable.setFont(new Font("Arial", 24));
+		labelUserTable.setUnderline(true);
+		labelUserTable.setStyle("-fx-font-weight: bold;");
 	    
 	    // Button to delete an user
 	    Button deleteUserButton = new Button("Delete user");
@@ -107,11 +136,11 @@ public class UsersManagement extends Application {
 	    		if(String.valueOf(user.getId()).toLowerCase().indexOf(lowerCaseFilter)  != -1) {
 	    			return true;
 	    		}
-	    		// We check if it's correspond to the user's firstname
+	    		// We check if it's correspond to the user's first name
 	    		else if(user.getFirstname().toLowerCase().indexOf(lowerCaseFilter) != -1) {
 	    			return true;
 	    		}
-	    		// We check if it's correspond to the user's lastname
+	    		// We check if it's correspond to the user's last name
 	    		else if(user.getLastname().toLowerCase().indexOf(lowerCaseFilter) != -1) {
 	    			return true;
 	    		}
@@ -132,92 +161,163 @@ public class UsersManagement extends Application {
 	    sortedData.comparatorProperty().bind(usersTable.comparatorProperty());
 	    usersTable.setItems(sortedData);
 	    
+	    searchTableButtonVBox.getChildren().addAll(filteredField, usersTable, deleteUserButton);
+	    
 	    // VBox containing the nodes for the users table
-	    VBox vbox = new VBox();
-	    vbox.setSpacing(5);
-	    vbox.setPadding(new Insets(10, 0, 0, 10));
-	    vbox.getChildren().addAll(label, filteredField, usersTable, deleteUserButton);
+	    VBox usersTableVBox = new VBox(20);
+	    usersTableVBox.setPadding(new Insets(10, 10, 10, 10));
+	    usersTableVBox.getChildren().addAll(labelUserTable, searchTableButtonVBox);
+	    usersTableVBox.setAlignment(Pos.CENTER);
 	    
-	    usersTablePane.getChildren().add(vbox);
+		return usersTableVBox;
+	}
+	
+	private Pane createUserCreationPane() {
+		// HBox containing the input for the user's information
+	    VBox userInfoInput = new VBox(25);
 	    
-	    // Users Creation pane
-	    usersCreationPane = new VBox();
+	    Label userCreationLabel = new Label("USER CREATION :");
+	    userCreationLabel.setFont(new Font("Arial", 24));
+	    userCreationLabel.setUnderline(true);
+	    userCreationLabel.setStyle("-fx-font-weight: bold;");
 	    
-	    // HBox containing the input for the user's information
-	    HBox userInfoInput = new HBox(10);
+	    Label userCreationExplanation = new Label("Fill in all the fields in order to create a new user.");
+	    
+	    // We create an HBox for each user's informations containing a label and a TextField
+	    HBox firstnameHBox = new HBox(28);
+	    HBox lastnameHBox = new HBox(27);
+	    HBox emailHBox = new HBox(47);
+	    
+	    Label firstnameLabel = new Label("• First name :");
+	    Label lastnameLabel = new Label("• Last name : ");
+	    Label emailLabel = new Label("• E-mail : ");
+	    
+	    firstnameLabel.setStyle("-fx-font-weight: bold;");
+	    lastnameLabel.setStyle("-fx-font-weight: bold;");
+	    emailLabel.setStyle("-fx-font-weight: bold;");
 	    
 	    TextField firstnameText = new TextField();
 	    TextField lastnameText = new TextField();
 	    TextField emailText = new TextField();
-	    Label userCreationLabel = new Label("User creation");
 	    
-	    firstnameText.setPromptText("Firstname");
-	    lastnameText.setPromptText("Lastname");
+	    firstnameText.setPromptText("First name");
+	    lastnameText.setPromptText("Last name");
 	    emailText.setPromptText("E-mail");
 	    
-	    userInfoInput.getChildren().addAll(firstnameText, lastnameText, emailText);
+	    emailText.setPrefWidth(250);
+	    
+	    firstnameHBox.getChildren().addAll(firstnameLabel, firstnameText);
+	    lastnameHBox.getChildren().addAll(lastnameLabel, lastnameText);
+	    emailHBox.getChildren().addAll(emailLabel, emailText);
 	    
 	    // Button for creating an user
 	    Button createUserButton = new Button("Create new user");
 	    createUserButton.setOnAction(new CreateUserButtonHandler(data, firstnameText, lastnameText, emailText));
+	    
+	    // We add all the node necessary to create an user 
+	    userInfoInput.getChildren().addAll(userCreationExplanation, firstnameHBox, lastnameHBox, emailHBox, createUserButton);
 
-	    usersCreationPane.getChildren().addAll(userCreationLabel, userInfoInput, createUserButton);
-
-	    // Users Modification pane
-	    usersModificationPane = new VBox();
-
-	    Label userModificationLabel = new Label("User modification");
-	    Label oldUsersFirstname = new Label("First name : ");
-	    Label oldUsersLastname = new Label("Last name : ");
-	    Label oldUsersEmail = new Label("E-mail : ");
+	    // VBox containing the nodes for the user creation
+	    VBox userCreationVBox = new VBox(20);
+	    userCreationVBox.setPadding(new Insets(10, 10, 10, 10));
+	    userCreationVBox.getChildren().addAll(userCreationLabel, userInfoInput);
+	    userCreationVBox.setAlignment(Pos.CENTER);
+	    
+		return userCreationVBox;
+	}
+	
+	private Node createUserModificationPane() {
+		// VBox containing all the old user's informations
+	    VBox usersOldInfos = new VBox(15);
+	    
+		Label userModificationLabel = new Label("USER MODIFICATION :");
+	    userModificationLabel.setFont(new Font("Arial", 24));
+	    userModificationLabel.setUnderline(true);
+	    userModificationLabel.setStyle("-fx-font-weight: bold;");
+	    
+	    Label oldUsersLabel = new Label("Previous user's information :");
+	    oldUsersLabel.setUnderline(true);
+	    oldUsersLabel.setStyle("-fx-font-weight: bold;");
+	    
+	    HBox oldUsersFirsnameAndValue = new HBox();
+	    HBox oldUsersLastnameAndValue = new HBox();
+	    HBox oldUsersEmailAndValue = new HBox();
+	    
+	    Label oldUsersFirstname = new Label("• First name : ");
+	    Label oldUsersLastname = new Label("• Last name : ");
+	    Label oldUsersEmail = new Label("• E-mail : ");
+	    
+	    oldUsersFirstname.setStyle("-fx-font-weight: bold;");
+	    oldUsersLastname.setStyle("-fx-font-weight: bold;");
+	    oldUsersEmail.setStyle("-fx-font-weight: bold;");
+	    
+	    Label oldUsersFirstnameValue = new Label("");
+	    Label oldUsersLastnameValue = new Label("");
+	    Label oldUsersEmailValue = new Label("");
 
 	    // If the selected user changed we update the values of the labels
 	    usersTable.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
 	        if (newSelection != null) {
-	        	oldUsersFirstname.setText("Firstname : " + usersTable.getSelectionModel().getSelectedItem().getFirstname());
-	        	oldUsersLastname.setText("Lastname : " + usersTable.getSelectionModel().getSelectedItem().getLastname());
-	        	oldUsersEmail.setText("E-mail : " + usersTable.getSelectionModel().getSelectedItem().getEmail());
+	        	oldUsersFirstnameValue.setText(usersTable.getSelectionModel().getSelectedItem().getFirstname());
+	        	oldUsersLastnameValue.setText(usersTable.getSelectionModel().getSelectedItem().getLastname());
+	        	oldUsersEmailValue.setText(usersTable.getSelectionModel().getSelectedItem().getEmail());
 	        }
 	    });
 	    
-	    // VBox containing all the old user's informations
-	    VBox userInfos = new VBox(10);
-	    userInfos.getChildren().addAll(userModificationLabel, oldUsersFirstname, oldUsersLastname, oldUsersEmail);
+	    oldUsersFirsnameAndValue.getChildren().addAll(oldUsersFirstname, oldUsersFirstnameValue);
+	    oldUsersLastnameAndValue.getChildren().addAll(oldUsersLastname, oldUsersLastnameValue);
+	    oldUsersEmailAndValue.getChildren().addAll(oldUsersEmail, oldUsersEmailValue);
+	    
+	    // We add the node useful for the old user's information
+	    usersOldInfos.getChildren().addAll(oldUsersLabel, oldUsersFirsnameAndValue, oldUsersLastnameAndValue, oldUsersEmailAndValue);
 	    
 	    // HBox containing all the new user's informations
-	    HBox newUserInfosInput = new HBox(10);
+	    HBox newUserInfosInput = new HBox(50);
+	    
+	    Label newUsersLabel = new Label("New user's information :");
+	    newUsersLabel.setUnderline(true);
+	    newUsersLabel.setStyle("-fx-font-weight: bold;");
 	    
 	    TextField newFirstnameText = new TextField();
 	    TextField newLastnameText = new TextField();
 	    TextField newEmailText = new TextField();
 	    
-	    newFirstnameText.setPromptText("New firstname");
-	    newLastnameText.setPromptText("New lastname");
+	    newEmailText.setPrefWidth(250);
+	    
+	    newFirstnameText.setPromptText("New first name");
+	    newLastnameText.setPromptText("New last name");
 	    newEmailText.setPromptText("New E-mail");
 	    
 	    // Button to modify an user
 	    Button modifyUserButton = new Button("Modify");
-	    modifyUserButton.setOnAction(new ModificationUserButtonHandler(oldUsersFirstname, oldUsersLastname, oldUsersEmail, newFirstnameText, newLastnameText, newEmailText, usersTable));
+	    modifyUserButton.setOnAction(new ModificationUserButtonHandler(oldUsersFirstnameValue, oldUsersLastnameValue, oldUsersEmailValue, newFirstnameText, newLastnameText, newEmailText, usersTable));
 
+	    Label userModificationExplanation = new Label("Fill in at least one field in order to modify an user.");
+	    
 	    // If no user is selected, the button is disable
 	    modifyUserButton.disableProperty().bind(Bindings.isEmpty(usersTable.getSelectionModel().getSelectedItems()));
 	    
+	    // We add the node useful for the new user's information
 	    newUserInfosInput.getChildren().addAll(newFirstnameText, newLastnameText, newEmailText, modifyUserButton);
 	    
-	    usersModificationPane.getChildren().addAll(userInfos, newUserInfosInput);
+	    // VBox containing all the old user's informations
+	    VBox usersNewInfos = new VBox(15);
+	    usersNewInfos.getChildren().addAll(newUsersLabel, userModificationExplanation, newUserInfosInput);
 	    
-	    // Main scene of the application
-	    usersBorderPane.setCenter(usersTablePane);
-	    usersBorderPane.setBottom(hboxButtonChangeScene);
-	    stage.setScene(new Scene(usersBorderPane));
-	    stage.show();
+	    // VBox containing the nodes for the user modification
+	    VBox usersModificationVBox = new VBox(25);
+	    usersModificationVBox.setPadding(new Insets(10, 10, 10, 10));
+	    usersModificationVBox.getChildren().addAll(userModificationLabel, usersOldInfos, usersNewInfos);
+	    usersModificationVBox.setAlignment(Pos.CENTER);
+		
+	    return usersModificationVBox;
 	}
-	
+
 	/**
 	 * Method to initialize the table's column with the user's values
 	 * @throws IOException
 	 */
-	public void initializeCol() throws IOException {
+	private void initializeCol() throws IOException {
 		usersBorderPane = new BorderPane();
 		
 		// We load the values of the users from the text file
@@ -240,12 +340,12 @@ public class UsersManagement extends Application {
 		idCol.setCellValueFactory(new PropertyValueFactory<User2, Integer>("id"));
 		
 		// Column for the user's firstname
-	    firstNameCol = new TableColumn<>("Firstname");
+	    firstNameCol = new TableColumn<>("First name");
 	    firstNameCol.setMinWidth(100);
 	    firstNameCol.setCellValueFactory(new PropertyValueFactory<User2, String>("firstname"));
 
 	    // Column for the user's lastnale
-	    lastNameCol = new TableColumn<>("Lastname");
+	    lastNameCol = new TableColumn<>("Last name");
 	    lastNameCol.setMinWidth(100);
 	    lastNameCol.setCellValueFactory(new PropertyValueFactory<User2, String>("lastname"));
 
