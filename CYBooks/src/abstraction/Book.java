cce246a450fe9ecf39803dde1edb2a83b1925335
Guillaume B <105287640+Auxifruit@users.noreply.github.com>
2024-05-21 15:47:22 +0200

    Nouvelle implémentation Book

--- CYBooks/src/abstraction/Book.java
+++ CYBooks/src/abstraction/Book.java
@@ -1,6 +1,8 @@
 package abstraction;
 
-import java.time.LocalDate;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 /**
  * The class Book represents a book in the library
@@ -8,56 +10,64 @@ import java.time.LocalDate;
  */
 public class Book {
 	protected static long MAX_BORROW_TIME = 30;
-	protected String isbn;
-	protected String title;
-	protected String author;
-	protected LocalDate publishedDate;
-	protected String theme;
-	protected String type;
-	protected String editor;
+	protected StringProperty identifier;
+	protected StringProperty title;
+	protected StringProperty author;
+	protected StringProperty publishedDate;
+	protected StringProperty format;
+	protected StringProperty type;
+	protected StringProperty publisher;
 	
 	/**
 	 * book constructor with all fields as parameters
-	 * @param isbn the book's isbn
+	 * @param identifier the book's identifier
 	 * @param title the book's title
 	 * @param author the book's author
 	 * @param publishedDate the book's published date
-	 * @param theme the book's theme
+	 * @param format the book's format
 	 * @param type the book's type
-	 * @param editor the book's editor
+	 * @param publisher the book's publisher
 	 */
-	public Book(String isbn, String title, String author, String publishedDate, String theme, String type, String editor) {
-		this.isbn = isbn;
-		this.title = title;
-		this.author = author;
-		this.publishedDate = LocalDate.parse(publishedDate);
-		this.theme = theme;
-		this.type = type;
-		this.editor = editor;
+	public Book(String identifier, String title, String author, String publishedDate, String format, String type, String publisher) {
+		this.identifier = new SimpleStringProperty(identifier);
+		this.title = new SimpleStringProperty(title);
+		this.author = new SimpleStringProperty(author);
+		this.publishedDate = new SimpleStringProperty(publishedDate);
+		this.format = new SimpleStringProperty(format);
+		this.type = new SimpleStringProperty(type);
+		this.publisher = new SimpleStringProperty(publisher);
 	}
 
 	/**
-     * get the book's isbn
-     * @return the book's isbn
+     * get the book's identifier
+     * @return the book's identifier
      */
-	public String getIsbn() {
-		return isbn;
+	public String getIdentifier() {
+		return identifier.get();
 	}
 
 	/**
-     * isbn setter method
-     * @param isbn the book's isbn
+     * identifier setter method
+     * @param identifier the book's identifier
      */
-	public void setIsbn(String isbn) {
-		this.isbn = isbn;
+	public void setIdentifier(String identifier) {
+		this.identifier.set(identifier);
 	}
+	
+	/**
+     * Method to return the StringProperty of the book's identifier for the TableView
+     * @return the StringProperty of the book's identifier 
+     */
+    public StringProperty identifierProperty() {
+        return identifier;
+    }
 
 	/**
      * get the book's title
      * @return the book's title
      */
 	public String getTitle() {
-		return title;
+		return title.get();
 	}
 
 	/**
@@ -65,15 +75,23 @@ public class Book {
      * @param title the book's title
      */
 	public void setTitle(String title) {
-		this.title = title;
+		this.title.set(title);
 	}
+	
+	/**
+     * Method to return the StringProperty of the book's title for the TableView
+     * @return the StringProperty of the book's title 
+     */
+    public StringProperty titleProperty() {
+        return title;
+    }
 
 	/**
      * get the book's author
      * @return the book's author
      */
 	public String getAuthor() {
-		return author;
+		return author.get();
 	}
 
 	/**
@@ -81,47 +99,71 @@ public class Book {
      * @param author the book's author
      */
 	public void setAuthor(String author) {
-		this.author = author;
+		this.author.set(author);
 	}
+	
+	/**
+     * Method to return the StringProperty of the book's author for the TableView
+     * @return the StringProperty of the book's author 
+     */
+    public StringProperty authorProperty() {
+        return author;
+    }
 
 	/**
      * get the book's published date
      * @return the book's published date
      */
-	public LocalDate getPublishedDate() {
-		return publishedDate;
+	public String getPublishedDate() {
+		return publishedDate.get();
 	}
 
 	/**
      * published date setter method
      * @param publishedDate the book's published date
      */
-	public void setPublishedDate(LocalDate publishedDate) {
-		this.publishedDate = publishedDate;
+	public void setPublishedDate(String publishedDate) {
+		this.publishedDate.set(publishedDate);
+	}
+	
+	/**
+     * Method to return the StringProperty of the book's published date for the TableView
+     * @return the StringProperty of the book's published date 
+     */
+    public StringProperty publishedDateProperty() {
+        return publishedDate;
+    }
+
+	/**
+     * get the book's format
+     * @return the book's format
+     */
+	public String getFormat() {
+		return format.get();
 	}
 
 	/**
-     * get the book's theme
-     * @return the book's theme
+     * format setter method
+     * @param format the book's format
      */
-	public String getTheme() {
-		return theme;
+	public void setFormat(String format) {
+		this.format.set(format);
 	}
-
+	
 	/**
-     * theme setter method
-     * @param theme the book's theme
+     * Method to return the StringProperty of the book's format for the TableView
+     * @return the StringProperty of the book's format 
      */
-	public void setTheme(String theme) {
-		this.theme = theme;
-	}
+    public StringProperty formatProperty() {
+        return format;
+    }
 
 	/**
      * get the book's type
      * @return the book's type
      */
 	public String getType() {
-		return type;
+		return type.get();
 	}
 
 	/**
@@ -129,23 +171,47 @@ public class Book {
      * @param type the book's type
      */
 	public void setType(String type) {
-		this.type = type;
+		this.type.set(type);
+	}
+	
+	/**
+     * Method to return the StringProperty of the book's type for the TableView
+     * @return the StringProperty of the book's type 
+     */
+    public StringProperty typeProperty() {
+        return type;
+    }
+
+	/**
+     * get the book's publisher
+     * @return the book's publisher
+     */
+	public String getPublisher() {
+		return publisher.get();
 	}
 
 	/**
-     * get the book's editor
-     * @return the book's editor
+     * publisher setter method
+     * @param publisher the book's publisher
      */
-	public String getEditor() {
-		return editor;
+	public void setPublisher(String publisher) {
+		this.publisher.set(publisher);
 	}
-
+	
 	/**
-     * editor setter method
-     * @param editor the book's editor
+     * Method to return the StringProperty of the book's publisher for the TableView
+     * @return the StringProperty of the book's publisher 
      */
-	public void setEditor(String editor) {
-		this.editor = editor;
+    public StringProperty publisherProperty() {
+        return publisher;
+    }
+	
+	/**
+	 * Get the maximum time a book can be borrow
+	 * @return the maximum time a book can be borrow
+	 */
+	public static long getMaxTimeBorrow() {
+		return MAX_BORROW_TIME;
 	}
 	
 	/**
@@ -155,13 +221,13 @@ public class Book {
 	@Override
 	public String toString() {
 		return "Book : " +
-				"\nISBN : " + getIsbn() +
+				"\nIdentifier : " + getIdentifier() +
 				"\nTitle : " + getTitle() +
 				"\nAuthor : " + getAuthor() +
 				"\nPublished date : " + getPublishedDate() +
-				"\nTheme : " + getTheme() +
+				"\nFormat : " + getFormat() +
 				"\nType : " + getType() + 
-				"\nEditor : " + getEditor();
+				"\nPublisher : " + getPublisher();
 	}
 	
 	/**
@@ -175,9 +241,9 @@ public class Book {
     		return false;
     	}
     	Book b = (Book) obj;
-    	return this.getIsbn() == b.getIsbn() && this.getTitle().equals(b.getTitle()) && this.getAuthor().equals(b.getAuthor())
-    			&& this.publishedDate.equals(b.getPublishedDate()) && this.getTheme().equals(b.getTheme()) && this.getType().equals(b.getType())
-    			&& this.getEditor().equals(b.getEditor());
+    	return this.getIdentifier() == b.getIdentifier() && this.getTitle().equals(b.getTitle()) && this.getAuthor().equals(b.getAuthor())
+    			&& this.getPublishedDate().equals(b.getPublishedDate()) && this.getFormat().equals(b.getFormat()) && this.getType().equals(b.getType())
+    			&& this.getPublisher().equals(b.getPublisher());
     }
 	
 }
